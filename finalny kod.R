bdcca186331cc9a3c0f93ea4d2d92c52e62b2fcf
Jakub Kołpaczyński <jakub.kolpaczynski@gmail.com>
2025-05-23 23:25:44 +0200

    dodanie wykresów

--- finalny kod.R
+++ finalny kod.R
@@ -61,3 +61,32 @@ colnames(ankieta)
 
 # --- Wyliczenie wskaźnika wypalenia ---
 ankieta$wypalenie_studenckie <- rowSums(ankieta[, 21:ncol(ankieta)], na.rm = TRUE)
+
+# --- Wykresy ---
+# Histogram
+ggplot(ankieta, aes(x = wypalenie_studenckie)) +
+  geom_histogram(binwidth = 1, fill = "blue", color = "black", alpha = 0.7) +
+  labs(title = "Histogram wypalenia studenckiego", x = "Wypalenie studenckie", y = "Liczba studentów") +
+  theme_minimal()
+# Wykres pudełkowy
+ggplot(ankieta, aes(x = Płeć.wartość, y = wypalenie_studenckie)) +
+  geom_boxplot(fill = "lightblue", color = "black", alpha = 0.7) +
+  labs(title = "Wykres pudełkowy wypalenia studenckiego w zależności od płci", 
+       x = "Płeć", y = "Wypalenie studenckie") +
+  theme_minimal()
+
+# --- Statystyki opisowe ---
+# Średnia i odchylenie standardowe
+srednia_wypalenie <- mean(ankieta$wypalenie_studenckie, na.rm = TRUE)
+odchylenie_wypalenie <- sd(ankieta$wypalenie_studenckie, na.rm = TRUE)
+srednia_wypalenie
+odchylenie_wypalenie
+#anova
+anova_model <- aov(wypalenie_studenckie ~ Płeć.wartość, data = ankieta)
+anova_table <- as.data.frame(summary(anova_model)[[1]])
+rownames(anova_table)[1] <- "Płeć"
+colnames(anova_table) <- c("Źródło", "Sumy kw.", "Stopnie sw.", "F", "p-wartość")
+
+kable(anova_table, caption = "Tabela ANOVA: ") %>%
+  kable_styling(bootstrap_options = c("striped", "hover", "bordered", "responsive"), 
+                full_width = FALSE, font_size = 14)
